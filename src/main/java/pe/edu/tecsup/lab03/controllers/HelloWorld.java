87a972de7dd4ec3eb8ccd60dbf8156369a9792b7
Chory <jery.becerra@tecsup.edu.pe>
2026-09-01 17:25:53 -0500

    siguiente commit

---
+++ src/main/java/pe/edu/tecsup/lab03/controllers/HelloWorld.java
@@ -0,0 +1,11 @@
+// StudentController.java
+package pe.edu.tecsup.lab03.controllers;
+import pe.edu.tecsup.lab03.services.StudentService;
+import pe.edu.tecsup.lab03.entities.StudentEntity;
+import java.util.List;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("¡Hola Mundo!");
+    }
+}
