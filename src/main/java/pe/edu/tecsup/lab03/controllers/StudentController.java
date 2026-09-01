609e5bfeb23f2b58fef91e3787562b8f32d05e0e
Chory <jery.becerra@tecsup.edu.pe>
2026-09-01 17:22:32 -0500

    continuando commit

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -1,10 +1,7 @@
-// StudentController.java
+
 package pe.edu.tecsup.lab03.controllers;
-import pe.edu.tecsup.lab03.services.StudentService;
-import pe.edu.tecsup.lab03.entities.StudentEntity;
-import java.util.List;
+
 
 public class StudentController {
-    private StudentService service = new StudentService();
-    public List<StudentEntity> listStudents() { return service.getAll(); }
+
 }
