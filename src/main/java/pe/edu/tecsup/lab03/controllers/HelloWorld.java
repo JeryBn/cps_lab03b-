35d128b694f89292772c27f1a2bb0436d403de53
Chory <jery.becerra@tecsup.edu.pe>
2026-09-01 17:36:28 -0500

    Recuperacion Commit

--- src/main/java/pe/edu/tecsup/lab03/controllers/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-
-package pe.edu.tecsup.lab03.controllers;
-
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("¡Hola Mundo!");
-    }
-}
---
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -0,0 +1,10 @@
+// StudentController.java
+package pe.edu.tecsup.lab03.controllers;
+import pe.edu.tecsup.lab03.services.StudentService;
+import pe.edu.tecsup.lab03.entities.StudentEntity;
+import java.util.List;
+
+public class StudentController {
+    private final StudentService service = new StudentService();
+    public List<StudentEntity> listStudents() { return service.getAll(); }
+}
